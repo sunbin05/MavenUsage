242fffe28086d5cdac6124dd279c9e7fbec83664
孙彬 <sunbin05@58ganji.com>
2019-09-12 14:54:35 +0800

    innit

---
+++ appService/src/main/java/com/mycompany/app/ServiceUtil.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class ServiceUtil {
+
+	public static void testUtil(){
+		System.out.println("test Util .... ");
+	}
+}
---
+++ appWeb/target/appWeb/WEB-INF/classes/demo/FirstDemo.java
@@ -0,0 +1,11 @@
+package demo;
+
+import com.mycompany.app.ServiceUtil;
+
+public class FirstDemo {
+	
+	public static void main(String[] args) {
+		ServiceUtil.testUtil();
+	}
+
+}
