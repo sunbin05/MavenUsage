f7eb84fc3dfd85873a5f9cd51de227f05f9b814c
sunbin05 <32047641+sunbin05@users.noreply.github.com>
2020-02-29 16:35:23 +0800

    Update generate_mvn_modules_project.sh

--- generate_sh/generate_mvn_modules_project.sh
+++ generate_sh/generate_mvn_modules_project.sh
@@ -17,3 +17,15 @@ cd myapp && sed -i "s/<packaging>jar<\/packaging>/<packaging>pom<\/packaging>/g"
 
 
 mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-WebApp -DarchetypeArtifactId=maven-archetype-webapp -DinteractiveMode=false
+
+本地项目直接推送到github
+mvn archetype:generate -DgrouId=a -DartifactId=b -Dversion=1.0.0.1
+..
+..
+cd /d/workspace/mavenUsage
+git init
+touch readme.txt
+git add *
+git commit * -m "init"
+git remote add origin git@github.com:sunbin05/MavenUsage.git
+git push -u origin master
